e5dc9c372b186b3a06eb9f319957e3684c40c3f4
MouseZhang <athope@163.com>
2018-01-29 20:04:05 +0800

    LinkList

---
+++ src/CH04/Node.java
@@ -0,0 +1,15 @@
+package CH04;
+
+public class Node {
+	public long data;
+	public Node next;
+	
+	public Node(long value) {
+		this.data = value;
+	}
+	
+	public void display() {
+		System.out.print(data + " ");
+	}
+
+}
