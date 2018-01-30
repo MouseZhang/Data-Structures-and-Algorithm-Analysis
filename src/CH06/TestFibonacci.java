5b132b610ba3f26b4838cbe760b5a97008ff47bd
MouseZhang <athope@163.com>
2018-01-30 11:46:00 +0800

    Basic of Recursion

---
+++ src/CH06/TestFibonacci.java
@@ -0,0 +1,8 @@
+package CH06;
+
+public class TestFibonacci {
+	public static void main(String[] args) {
+		System.out.println(Fibonacci.getNumber(5));
+	}
+
+}
---
+++ src/CH06/TestTriangle.java
@@ -0,0 +1,10 @@
+package CH06;
+
+public class TestTriangle {
+	public static void main(String[] args) {
+		System.out.println(Triangle.getNumber(500));
+		System.out.println(Triangle.getNumberByRecursion(500));
+		
+	}
+
+}
