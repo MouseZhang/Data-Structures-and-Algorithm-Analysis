749a3577b7b62f060aacc7c3c3683c384a7a80fc
MouseZhang <athope@163.com>
2018-01-31 16:46:29 +0800

    BinaryTree ADT

---
+++ src/CH11/Node.java
@@ -0,0 +1,21 @@
+package CH11;
+/**
+ * 二叉树节点
+ * @author MouseZhang
+ *
+ */
+public class Node {
+	public long data;
+	public String sData;
+	public Node leftChild;
+	public Node rightChild;
+	
+	/**
+	 * 构造方法
+	 * @param data
+	 */
+	public Node(long data, String sData) {
+		this.data = data;
+		this.sData = sData;
+	}
+}
