3aee601d1968a2aa2bae4813d9312784780d1ed7
MouseZhang <athope@163.com>
2018-01-31 15:52:36 +0800

    the concept of BinaryTree

---
+++ src/CH10/Node.java
@@ -0,0 +1,19 @@
+package CH10;
+/**
+ * 二叉树节点
+ * @author MouseZhang
+ *
+ */
+public class Node {
+	public long data;
+	public Node leftChild;
+	public Node rightChild;
+	
+	/**
+	 * 构造方法
+	 * @param data
+	 */
+	public Node(long data) {
+		this.data = data;
+	}
+}
---
+++ src/CH10/Tree.java
@@ -0,0 +1,35 @@
+package CH10;
+/**
+ * 二叉树类
+ * @author MouseZhang
+ *
+ */
+public class Tree {
+	// 根节点
+	private Node root;
+	
+	/**
+	 * 插入节点
+	 * @param value
+	 */
+	public void insert(long value) {
+		
+	}
+	
+	/**
+	 * 查找节点
+	 * @param value
+	 */
+	public void find(long value) {
+		
+	}
+	
+	/**
+	 * 删除节点
+	 * @param value
+	 */
+	public void delete(long value) {
+		
+	}
+
+}
