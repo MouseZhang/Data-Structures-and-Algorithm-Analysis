927e69c801b8fd26414c3f5a4a1c484d790fe777
MouseZhang <athope@163.com>
2018-02-03 23:08:19 +0800

    open addressing

---
+++ src/CH16/TestHashTable.java
@@ -0,0 +1,19 @@
+package CH16;
+
+public class TestHashTable {
+	public static void main(String[] args) {
+		HashTable ht = new HashTable();
+		ht.insert(new Info("tom", "汤姆"));
+		ht.insert(new Info("a", "杰克"));
+		ht.insert(new Info("ct", "约翰"));
+		ht.insert(new Info("b", "麦克"));
+		
+		System.out.println(ht.find("a").getName());
+		System.out.println(ht.find("ct").getName());
+		System.out.println(ht.find("b").getName());
+		
+		ht.delete("b");
+		System.out.println(ht.find("b").getName());
+	}
+
+}
