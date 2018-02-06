e9c7adb834a9d6eb7c5b8bb730cfaa63c083bb39
MouseZhang <athope@163.com>
2018-02-06 14:01:56 +0800

    Stack and Queue

--- src/CH03/MyCycleQueue.java
+++ src/CH03/MyCycleQueue.java
@@ -1,11 +1,22 @@
 package CH03;
-
+/**
+ * 队列类
+ * @author MouseZhang
+ *
+ */
 public class MyCycleQueue {
+	//底层使用数组
 	private long[] arr;
+	//有效数据的大小
 	private int elements;
+	//队头
 	private int front;
+	//队尾
 	private int end;
 	
+	/**
+	 * 默认构造方法
+	 */
 	public MyCycleQueue() {
 		arr = new long[10];
 		elements = 0;
@@ -13,6 +24,10 @@ public class MyCycleQueue {
 		end = -1;
 	}
 	
+	/**
+	 * 带参数的构造方法，参数为数组的大小
+	 * @param maxSize
+	 */
 	public MyCycleQueue(int maxSize) {
 		arr = new long[maxSize];
 		elements = 0;
@@ -20,6 +35,10 @@ public class MyCycleQueue {
 		end = -1;
 	}
 	
+	/**
+	 * 添加数据，从队尾插入
+	 * @param value
+	 */
 	public void insert(long value) {
 		if (end == arr.length - 1) {
 			end = -1;
@@ -28,6 +47,10 @@ public class MyCycleQueue {
 		elements++;
 	}
 	
+	/**
+	 * 删除数据，从队头删除
+	 * @return
+	 */
 	public long remove() {
 		long value = arr[front++];
 		if (front == arr.length) {
@@ -37,14 +60,26 @@ public class MyCycleQueue {
 		return value;
 	}
 	
+	/**
+	 * 查看数据，从队头查看
+	 * @return
+	 */
 	public long peek() {
 		return arr[front];
 	}
 	
+	/**
+	 * 判断是否为空
+	 * @return
+	 */
 	public boolean isEmpty() {
 		return elements == 0;
 	}
 	
+	/**
+	 * 判断是否队满
+	 * @return
+	 */
 	public boolean isFull() {
 		return elements == arr.length;
 	}
--- src/CH03/MyQueue.java
+++ src/CH03/MyQueue.java
@@ -1,11 +1,22 @@
 package CH03;
-
+/**
+ * 队列类
+ * @author MouseZhang
+ *
+ */
 public class MyQueue {
+	//底层使用数组
 	private long[] arr;
+	//有效数据的大小
 	private int elements;
+	//队头
 	private int front;
+	//队尾
 	private int end;
 	
+	/**
+	 * 默认构造方法
+	 */
 	public MyQueue() {
 		arr = new long[10];
 		elements = 0;
@@ -13,6 +24,10 @@ public class MyQueue {
 		end = -1;
 	}
 	
+	/**
+	 * 带参数的构造方法，参数为数组的大小
+	 * @param maxSize
+	 */
 	public MyQueue(int maxSize) {
 		arr = new long[maxSize];
 		elements = 0;
@@ -20,24 +35,44 @@ public class MyQueue {
 		end = -1;
 	}
 	
+	/**
+	 * 添加数据，从队尾插入
+	 * @param value
+	 */
 	public void insert(long value) {
 		arr[++end] = value;
 		elements++;
 	}
 	
+	/**
+	 * 删除数据，从队头删除
+	 * @return
+	 */
 	public long remove() {
 		elements--;
 		return arr[front++];
 	}
 	
+	/**
+	 * 查看数据，从队头查看
+	 * @return
+	 */
 	public long peek() {
 		return arr[front];
 	}
 	
+	/**
+	 * 判断是否为空
+	 * @return
+	 */
 	public boolean isEmpty() {
 		return elements == 0;
 	}
 	
+	/**
+	 * 判断是否队满
+	 * @return
+	 */
 	public boolean isFull() {
 		return elements == arr.length;
 	}
