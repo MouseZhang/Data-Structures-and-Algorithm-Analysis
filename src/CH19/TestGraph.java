531dcd39ca7a9a6c082e32ac2e733913107feb7f
MouseZhang <athope@163.com>
2018-02-06 10:44:43 +0800

    Depth First Search and Breadth First Search

--- src/CH19/TestGraph.java
+++ src/CH19/TestGraph.java
@@ -15,6 +15,28 @@ public class TestGraph {
 		g.addEdge(3, 4);
 		
 		g.dfs();
+		
+		System.out.println();
+		
+		Graph h = new Graph();
+		h.addVertex('A');
+		h.addVertex('B');
+		h.addVertex('C');
+		h.addVertex('D');
+		h.addVertex('E');
+		h.addVertex('F');
+		h.addVertex('G');
+		h.addVertex('H');
+		
+		h.addEdge(0, 1);
+		h.addEdge(0, 2);
+		h.addEdge(0, 3);
+		h.addEdge(1, 4);
+		h.addEdge(4, 5);
+		h.addEdge(2, 6);
+		h.addEdge(3, 7);
+		
+		h.bfs();
 	}
 
 }
